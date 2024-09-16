264fefc63ff4af950989f841203c2472f297ff97
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2024-09-16 10:02:01 +1000

    adding...

--- src/main/scala/ScalaProgrammingV4/Chapter6/Rational.scala
+++ src/main/scala/ScalaProgrammingV4/Chapter6/Rational.scala
@@ -1,21 +1,43 @@
 package ScalaProgrammingV4.Chapter6
 
+implicit def IntToRational(x:Int):Rational=new Rational(x)
+
 class Rational(n:Int,d:Int){
   require(d!=0)
 
 
-  private val g = gcd(n,d)
+  private val g = gcd(n,d).abs
   val numer = n/g
   val denom = d/g
-  println("Created "+numer+"/"+denom)
+  //println("Created "+numer+"/"+denom)
 
   override def toString=s"$numer/$denom"
 
-  def add(that:Rational):Rational=
+  def +(that:Rational):Rational=
     new Rational(
       numer * that.denom + that.numer*denom,
       denom * that.denom
     )
+  def +(that:Int):Rational=this + (new Rational(that))
+  def -(that:Rational):Rational=
+    new Rational(
+      numer * that.denom - that.numer*denom,
+      denom * that.denom
+    )
+  def -(that:Int):Rational=this - (new Rational(that))
+
+  def *(that:Rational):Rational=
+    new Rational(
+      numer * that.numer,
+      denom * that.denom
+    )
+  def *(that:Int):Rational= this * (new Rational(that))
+  def /(that:Rational):Rational=
+    new Rational(
+      numer * that.denom,
+      denom * that.numer
+    )
+  def /(that:Int):Rational= this / (new Rational(that))
 
   def lessThan(that:Rational)=
     numer * that.denom < that.numer * denom
@@ -34,10 +56,18 @@ object RationalRun extends App{
 
   val half=new Rational(1,2)
   val quarter = new Rational(1,4)
-  println(half add quarter)
+  println(half + quarter)
   println(half max quarter)
   val five=new Rational(5)
 
+  val a = half + quarter * half
+  val b = half * quarter
+
+  val c = a / 5
+  val d = b * 5
+  println(a)
+  println(c)
+  println(5 / a)
 }
 
 
